7d6f52c88c272fd2195538dd158c553c5cd38ead
Alaury <AlauryJ@gmail.com>
2021-03-04 03:31:54 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p> My name is Alaury and I'm going to master React!
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/alauryj"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
